9aa3dbb70393d60f3bfc0a9e91d2213f592ce48a
FireWolfy89 <dmate899@gmail.com>
2024-04-07 21:33:52 +0200

    Goals Window in progress

--- CLogger/App.xaml.cs
+++ CLogger/App.xaml.cs
@@ -10,9 +10,9 @@ namespace CLogger
     /// </summary>
     public partial class App : Application
     {
-       static  string databaseName = "Macros.db";
-       static  string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-       public static  string databasePath = System.IO.Path.Combine(folderPath, databaseName);
+        static string databaseName = "Macros.db";
+        static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string databasePath = System.IO.Path.Combine(folderPath, databaseName);
     }
 
     public partial class DB2 : Application
@@ -24,7 +24,8 @@ namespace CLogger
 
     public partial class DB3 : Application
     {
-        static string databaseName = "Weeks.db";
+        static string databaseName = "Goals.db";
+
         static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         public static string databasePath = System.IO.Path.Combine(folderPath, databaseName);
     }
